630257b6331123b70b39aa836fb73426cea5fd42
Yefry <yefry123crack@gmail.com>
2026-06-17 14:51:03 -0500

    feat: conecte navegacion en MainActivity

--- app/src/main/java/com/tuequipo/autocare/MainActivity.kt
+++ app/src/main/java/com/tuequipo/autocare/MainActivity.kt
@@ -4,44 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.tuequipo.autocare.data.local.database.AutoCareDatabase
+import com.tuequipo.autocare.data.remote.api.RetrofitClient
+import com.tuequipo.autocare.data.repository.AutoCareRepository
+import com.tuequipo.autocare.ui.navigation.NavGraph
 import com.tuequipo.autocare.ui.theme.AutoCareTheme
+import com.tuequipo.autocare.viewmodel.AutoCareViewModelFactory
 
 class MainActivity : ComponentActivity() {
+    private lateinit var viewModelFactory: AutoCareViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val database = AutoCareDatabase.getDatabase(applicationContext)
+        val repository = AutoCareRepository(
+            vehiculoDao = database.vehiculoDao(),
+            mantenimientoDao = database.mantenimientoDao(),
+            carApiService = RetrofitClient.carApiService
+        )
+        viewModelFactory = AutoCareViewModelFactory(repository)
+
         setContent {
             AutoCareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph(factory = viewModelFactory)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AutoCareTheme {
-        Greeting("Android")
     }
 }
